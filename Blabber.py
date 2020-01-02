e63c0e7f212cafcd5e2eb2965d188a3829563ca3
jackyEightz <10355487+jackyEightz@users.noreply.github.com>
2020-01-01 17:32:40 -0800

    added some branches to prevent exceptions for connection and disconnect

--- Blabber.py
+++ Blabber.py
@@ -1,4 +1,4 @@
-#Blabber
+#Blabber.py
 '''Created by:
 Fanny Avila (Fa-Avila)
 Marcos Avila (DaiconV),
@@ -14,24 +14,45 @@ TOKEN = os.getenv('discord_token')
 
 bot = commands.Bot(command_prefix='>')
 
-#check to see if the bot is functional
+#in the future add a help function
+
+#command for testing purposes, to check to see if the bot is functional
 @bot.command(name='hello')
 async def greeting(ctx):
-    await ctx.send('greeting::hello invoked')
+    await ctx.send('Blabber::hello invoked')
     await bot.close()
 
-#connect bot to the voice chat that the user is in
-@bot.command(name='connect')
-async def connectToVC(ctx):
-    voiceChannel = ctx.message.author.voice.channel
-    await ctx.send(f'connectToVC::TestDiscodPyBot connecting to {voiceChannel.name}')
-    await voiceChannel.connect()
-
-#disconnect bot from voice chat that user is in
+#command for testing purposes, to check to see if the bot prints out the username (name) and nickname(display name)
+@bot.command(name='user')
+async def user(ctx):
+    await ctx.send(f'Blabber::user hello user {ctx.author.name} or should I say {ctx.author.display_name}')
+    await bot.close()
+    
+#command to disconnect bot from voice chat that user is in
 @bot.command(name='disconnect')
 async def disconnectFromVC(ctx):
-    voiceClient = ctx.voice_client
-    await ctx.send(f'disconnectFromVC::TestDiscodPyBot disconnecting from {voiceClient.channel}')
-    await voiceClient.disconnect()
-    
+    if ctx.voice_client == None:
+        await ctx.send('Blabber::disconnectFromVC Blabber is not connected')
+    elif len(ctx.voice_client.channel.members) == 1 or ctx.author in ctx.voice_client.channel.members:
+        voiceClient = ctx.voice_client
+        await ctx.send(f'Blabber::disconnectFromVC disconnecting from {voiceClient.channel}')
+        await voiceClient.disconnect()
+    else:
+        await ctx.send('Blabber::disconnectFromVC You need to be in a voice channel to use this command.')
+
+#command to connect bot to the voice chat that the user is in
+@bot.command(name='connect')
+async def connectToVC(ctx):
+    if ctx.author.voice.channel == None:
+        await ctx.send('Blabber::connectToVC You need to be in a voice channel to use this command.') 
+    elif ctx.voice_client == None:
+        voiceChannel = ctx.author.voice.channel
+        await ctx.send(f'Blabber::connectToVC connecting to {voiceChannel.name}')
+        await voiceChannel.connect()
+    elif ctx.voice_client.channel == ctx.author.voice.channel:
+        await ctx.send(f'Blabber::connectToVC already connected to {voiceChannel.name}')
+    elif len(ctx.voice_client.channel.members) == 1:
+        await ctx.send(f'Blabber::connectToVC swaping from {ctx.voice_client.channel} to {ctx.author.voice.channel.name}')
+        await ctx.voice_client.move_to(ctx.author.voice.channel)
+
 bot.run(TOKEN)
