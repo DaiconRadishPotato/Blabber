7e33a578846fec15aa1af4511293c6b5e9d51f61
jackyEightz <10355487+jackyEightz@users.noreply.github.com>
2020-04-18 19:32:02 -0700

    fixed spacing, editted exception handling, and editted docstrings

--- cogs/profiles.py
+++ cogs/profiles.py
@@ -4,7 +4,7 @@
 # Contributor:  Jacky Zhang (jackyeightzhang),
 #               Marcos Avila (DaiconV)
 # Date created: 3/27/2020
-# Date last modified: 4/17/2020
+# Date last modified: 4/18/2020
 # Python Version: 3.8.1
 # License: MIT License
 
@@ -21,7 +21,9 @@ class Profiles(commands.Cog):
     user's voice profiles
     
     attributes:
+        DEFAULT_VOICE [tuple]: a constant tuple of voice arguments
         bot [discord.Bot]: discord Bot object
+        aliases [set]: set object of string alias/voice names
     """
     def __init__(self, bot):
         self.DEFAULT_VOICE = (
@@ -49,34 +51,37 @@ class Profiles(commands.Cog):
         parameter:
             ctx [commands.Context]: discord Contxt object
             alias [str]: string representing a specific alias
+        raises:
+            MissingRequiredArgument: an alias was not passed as an argument
         """
         alias=alias.lower()
         
         if alias == self.DEFAULT_VOICE[0]:
             try:
-                uds=UserDataService()
+                uds = UserDataService()
                 uds.remove_voice(ctx.author.id, ctx.channel.id)
                 await ctx.send(f":white_check_mark: "
                     f"**The new voice is** '{alias}'")
-            except:
+            except Exception as e:
                 await ctx.send(f":x: "
-                    f"**Had trouble setting up the new voice.**")
+                "**Had trouble setting up the new voice.**")
                 await ctx.send(f"Try again at a later time")
+                raise e
                 
-        elif (alias in self.aliases):
+        elif alias in self.aliases:
             try:
-                uds=UserDataService()
+                uds = UserDataService()
                 uds.set_voice(ctx.author.id, ctx.channel.id, alias)
                 await ctx.send(f":white_check_mark: "
-                    f"**The new voice is **'{alias}'")
-            except:
+                f"**The new voice is **'{alias}'")
+            except Exception as e:
                 await ctx.send(f":x: "
-                    f"**Had trouble setting up the new voice.**")
+                f"**Had trouble setting up the new voice.**")
                 await ctx.send(f"Try again at a later time")
-                
+                raise e
         else:
             await ctx.send(f":x: **`{alias}` is not a valid voice**")
-            
+
     async def _get_voice(self, user_id, channel_id):
         """
         Prints current users current voice profile.
@@ -92,19 +97,25 @@ class Profiles(commands.Cog):
             if voice is None:
                 voice = self.DEFAULT_VOICE
             return voice
-        except:
-            pass
+        except Exception as e:
+            raise e
             
     @set_voice.error
     async def set_voice_error(self, ctx, error):
         """
         Sends informational embed to be displayed if set voice command
         is being missing arguments
+        
+        parameters:
+            ctx [commands.Context]: discord Context object
+            error [Error]: general Error object
         """
         if isinstance(error, commands.MissingRequiredArgument):
-            voice = (await self._get_voice(ctx.author.id, ctx.channel.id))[0] #cache?
-            prefix= (await self.bot.get_cog("Settings")._get_prefix(ctx.guild.id))
-            member=ctx.message.author
+            voice = (await self._get_voice(ctx.author.id, ctx.channel.id))[0]
+            prefix = (await self.bot.get_cog("Settings"
+            )._get_prefix(ctx.guild.id))
+            member = ctx.message.author
+            
             embed = Embed(title="Blabber Voice",
             description="Changes the voice used to send messages in "
             "Blabber bot. You can also use the bot by @mentioning it " 
@@ -114,10 +125,13 @@ class Profiles(commands.Cog):
             
             embed.add_field(name=f"{member}'s current Voice:", 
             value=f"`{voice}`")
+
             embed.add_field(name="Update Voice:", 
             value=f"`{prefix}v [new voice]`")
-            await ctx.send(embed=embed)
             
+            await ctx.send(embed=embed)
+
+
 def setup(bot):
     """
     Adds Settings Cog to bot.
