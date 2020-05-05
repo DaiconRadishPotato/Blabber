fbdbbfe9b70195d66971c68ca8435237e008e74f
Marcos A. Avila <marcosa@uci.edu>
2020-05-05 08:23:24 -0700

    Restructuring check system.

--- blabber/checks.py
+++ blabber/checks.py
@@ -12,81 +12,52 @@ from discord.ext import commands
 
 from blabber.errors import *
 
-def is_guild_owner(ctx):
-    """
-    Checks if invoker is the owner of the guild.
+async def _can_disconnect(ctx):
+    # Count number of users in voice channel excluding context author and bots
+    user_count = 0
+    for member in ctx.voice_client.channel.members:
+        user_count += (not member.bot and member.id != ctx.author.id)
 
-    parameter:
-        ctx [commands.Context]: discord Context object
-    returns:
-        boolean: True if invoker is owner of guild
-    """
-    return ctx.author == ctx.guild.owner
-
-def is_connected():
-    async def predicate(ctx):
-        if ctx.author.voice:
-            return True
-        else:
-            raise NotConnected()
-    return commands.check(predicate)
-
-def bot_is_connected():
-    async def predicate(ctx):
-        if ctx.voice_client:
-            return True
-        else:
-            raise BotNotConnected()
-    return commands.check(predicate)
-
-def bot_can_disconnect():
-    async def predicate(ctx):
-        await bot_is_connected().predicate(ctx)
-
-        # Count number of users in voice channel excluding context author and bots
-        user_count = 0
-        for member in ctx.voice_client.channel.members:
-            user_count += (not member.bot and member.id != ctx.author.id)
-
-        if user_count == 0:
-            return True
-
+    if user_count > 0:
         try:
-            await commands.has_role('Blabby').predicate(ctx)
-            await commands.has_permissions(manage_channels=True).predicate(ctx)
+            await commands.check_any(
+                commands.has_role('Blabby').predicate(ctx), 
+                commands.has_permissions(manage_channels=True).predicate(ctx))
         except:
             raise MissingCredentials()
-        else:
-            return True
 
+
+async def _blabber_has_required_permissions(ctx):
+    bot = ctx.guild.get_member(ctx.bot.user.id)
+    bot_permissions = ctx.author.voice.channel.permissions_for(bot)
+
+    if not bot_permissions.connect:
+        raise BlabberMissingConnectPermission(ctx.author.voice.channel.name)
+    elif not bot_permissions.speak:
+        raise BlabberMissingSpeakPermission(ctx.author.voice.channel.name)
+
+
+def connect_check():
+    async def predicate(ctx):
+        if not ctx.author.voice:
+            raise NotConnected()
+        elif ctx.author.voice is not ctx.voice_client:
+            await _blabber_has_required_permissions(ctx)
+            await _can_disconnect(ctx)
+        return True
     return commands.check(predicate)
 
-def bot_can_connect():
+def disconnect_check():
     async def predicate(ctx):
-        await is_connected().predicate(ctx)
-
-        bot = ctx.guild.get_member(ctx.bot.user.id)
-        bot_permissions = ctx.author.voice.channel.permissions_for(bot)
-
-        if bot_permissions.connect and bot_permissions.speak:
-            return True
-        else:
-            raise BotMissingVoiceChannelPermissions(ctx.author.voice.channel.name)
-
+        if not ctx.voice_client:
+            raise BlabberNotConnected()
+        await _can_disconnect(ctx)
+        return True
     return commands.check(predicate)
 
-def bot_can_move():
+def say_check()
     async def predicate(ctx):
-        await is_connected().predicate(ctx)
-
-        try:
-            await bot_can_connect().predicate(ctx)
-            await bot_can_disconnect().predicate(ctx)
-        except BotMissingVoiceChannelPermissions as e:
-            raise e
-        except:
-            raise BotConnectedToAnotherChannel()
-        else:
-            return True
-
-    return commands.check(predicate) 
+        if ctx.message.content > 600
+            raise MessageTooLong()
+        return True
+    return commands.check(predicate)
