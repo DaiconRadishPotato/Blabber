3f9eb58fbec15385131afaaaa2a783212992e14d
jackyEightz <10355487+jackyEightz@users.noreply.github.com>
2020-05-26 14:44:23 -0700

    Changed docstrings and added comments

--- cogs/settings.py
+++ cogs/settings.py
@@ -24,7 +24,6 @@ class Settings(commands.Cog):
     """
 
     def __init__(self, bot):
-        self.bot = bot
         self.prefixes = bot.prefixes
 
     @commands.group(name='settings')
@@ -34,17 +33,18 @@ class Settings(commands.Cog):
         parameter.
 
         parameter:
-            ctx [commands.Context]: discord Contxt object
+            ctx [Context]: discord Contxt object
         """
+        # Check if user invoked a subcommand
         if ctx.invoked_subcommand is None:
+            embed = Embed(
+                title="Blabber Settings - Menu",
+                description="Use the command format `{prefix}settings [option]`"
+                "to view more info about an option.",
+                colour=Colour.blue())
+
+            # Generate options menu
             prefix = await self._get_prefix(ctx.guild)
-
-            embed = Embed(title="Blabber Settings - Menu",
-                          description="Use the command format "
-                          f"`{prefix}settings [option]` "
-                          "to view more info about an option.",
-                          colour=Colour.blue())
-
             embed.add_field(name="Prefix", value=f"`{prefix}settings prefix`")
 
             await ctx.send(embed=embed)
@@ -53,11 +53,10 @@ class Settings(commands.Cog):
     @commands.check(is_guild_owner)
     async def set_prefix(self, ctx, prefix: str):
         """
-        Changes prefix of Blabber for a particular guild. Displays a message
-        to user if set was successful or failed.
+        Changes prefix used to invoke Blabber for a particular guild.
 
         parameter:
-            ctx [commands.Context]: discord Contxt object
+            ctx [Context]: discord Contxt object
             prefix [str]: string used before commands to invoke blabber bot
         raises:
             MissingRequiredArgument: New prefix was not passed as an argument
@@ -65,27 +64,9 @@ class Settings(commands.Cog):
         self.prefixes[ctx.guild] = prefix
         await ctx.send(f":white_check_mark: **The new prefix is **'{prefix}'")
 
-    async def check_prefix(self, bot, message):
-        """
-        Determines whether a user message is a command by checking if it has a
-        specified prefix or @mention
-
-        parameters:
-            bot [discord.Bot]: discord Bot object
-            message [discord.Message]: the message or context from the guild
-                                       that called the bot.
-        returns:
-            prefix [str]: that is used to call commands from the bot client
-        """
-        return commands.when_mentioned_or(
-            await self._get_prefix(message.guild))(bot, message)
-
-
-
     async def _get_prefix(self, guild):
         """
-        Checks database if guild has a different prefix than the default.
-        Returns DEFAULT_PREFIX if record not found on the database
+        Checks cache and database for the guilds prefix.
 
         parameters:
             guild [Guild]: discord Guild object
@@ -95,23 +76,32 @@ class Settings(commands.Cog):
         prefix = self.prefixes[guild]
         return prefix
 
+    async def check_prefix(self, bot, message):
+        """
+        Determines whether a user message is a command by checking if it has a
+        specified prefix or @mention
+
+        parameters:
+            bot [Bot]: discord Bot object
+            message [Message]: the message or context from the guild that 
+                               called the bot.
+        returns:
+            prefix [str]: that is used to call commands from the bot client
+        """
+        return commands.when_mentioned_or(
+            await self._get_prefix(message.guild))(bot, message)
 
     @set_prefix.error
     async def set_prefix_error(self, ctx, error):
         """
-        Sends informational embed to be displayed if set prefix command
-        is missing arguments.
-        If invoker does not pass a prefix, then send current prefix, show
-        set_prefix command format, and describe prefix requirement.
+        Sends informational embed if set prefix command is missing arguments.
 
         parameters:
-            ctx [commands.Context]: discord Context object
+            ctx [Context]: discord Context object
             error [Error]: general Error object
         """
-
+        # Checks if no paramerter was passed to cause error
         if isinstance(error, commands.MissingRequiredArgument):
-            prefix = await self._get_prefix(ctx.guild)
-
             embed = Embed(
                 title="Blabber Settings - Prefix",
                 description="Changes the prefix used to command Blabber bot. "
@@ -119,14 +109,12 @@ class Settings(commands.Cog):
                 "the prefix.",
                 colour=Colour.blue())
 
+            # Generate prefix information for embed
+            prefix = await self._get_prefix(ctx.guild)
             embed.add_field(name="Current Prefix:",
                             value=f"`{prefix}`")
-
-            embed.add_field(name="Update Prefix:",
+            embed.add_field(name="Update Prefix (max 5 characters):",
                             value=f"`{prefix}settings prefix [new prefix]`")
-
-            embed.add_field(name="Valid Prefix Reqs:",
-                            value=f"`Any text, max 5 characters`")
 
             await ctx.send(embed=embed)
 
