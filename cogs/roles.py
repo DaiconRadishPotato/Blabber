0f434e09a4446e881df9d6c7328c5c969e1ce078
Marcos A. Avila <marcosa@uci.edu>
2020-06-02 22:04:40 -0700

    Finished implementing UX design to Roles cog.

--- cogs/roles.py
+++ cogs/roles.py
@@ -4,21 +4,20 @@
 # Contributor:  Fanny Avila (Fa-Avila),
 #               Marcos Avila (DaiconV)
 # Date created: 2/3/2019
-# Date last modified: 5/29/2020
+# Date last modified: 6/2/2020
 # Python Version: 3.8.1
 # License: MIT License
 
-from discord import utils, Embed, Colour
+from discord import utils, Embed, Colour, Member
 from discord.ext import commands
 
 from blabber.checks import *
 from blabber.errors import *
 
 
-class _Roles(commands.Cog):
+class Roles(commands.Cog):
     """
-    Private Permission Cog that allows role managers to give permission for 
-    certain users to use blabber
+    Collection of commands for managing Guild roles.
 
     parameters:
         bot [Bot]: client object representing a Discord bot
@@ -26,80 +25,69 @@ class _Roles(commands.Cog):
     def __init__(self, bot):
         self.prefixes = bot.prefixes
 
-    @commands.command(name="give_blabby", aliases=['gb'])
+    @commands.command(name="giveblabby", aliases=['gb'])
     @commands.has_permissions(manage_roles=True)
-    async def give_blabby(self, ctx, *, user: str=''):
+    @commands.bot_has_permissions(manage_roles=True)
+    async def giveblabby(self, ctx, *, member_name: str=''):
         """
-        Gives Blabby role to inputted user, which allows user to have priority
-        when using blabber bot commands.
-
-        parameters:
-            ctx [Context]: context object produced by a command invocation
-            user    [str] (default=''): string object representing a username
-                                        or nickname
-        """
-        # Check if user was provided
-        if not user:
-            embed = Embed(title=":x: Unable to give Blabby role",
-                          description="You must input a user or nickname to "
-                          "use this command",
-                          colour=Colour.red())
-        else:
-            # Ensure that user exists
-            member = utils.find(
-                lambda m: m.name == user or m.nick == user,
-                ctx.guild.members)
-            await user_is_valid(user, member)
-
-            # Ensure that Blabby role exists
-            blabby_role = utils.get(ctx.guild.roles, name="Blabby")
-            if not blabby_role:
-                blabby = await ctx.guild.create_role(
-                    name='Blabby', 
-                    reason="To allow certain people to use Blabber Bot.")
-
-            await member.add_roles(blabby_role)
-
-            embed = Embed(title=f":white_check_mark: {user} has been given "
-                                 "`Blabby` role",
-                          colour=Colour.green())
-        await ctx.send(embed=embed)
-    
-    @give_blabby.error
-    async def give_blabby_error(self, ctx, error):
-        """
-        Local error handler for give_blabby.
-        If user parameter is invalid, send an error message.
-        If invoker does not have permission to use command, send error message.
+        Assigns Blabby role to a server member.
 
         parameters:
             ctx     [Context]: context object produced by a command invocation
-            error [Exception]: error object thrown by command function
+            member  [str] (default=''): nickname/username of member in guild
         """
-        if isinstance(error, InvalidUser):
-            prefix = self.prefixes[ctx.guild]
-            embed = Embed(title=":x: Unable to give Blabby role",
-                          description="",
-                          colour=Colour.red())
-            embed.add_field(name="Input User:",
-                            value=f"{error}",
-                            inline=False)
-            embed.add_field(name="To user give_blabby:",
-                            value=f"'{prefix}give_blabby [user/nickname]'",
-                            inline=False)
-        elif isinstance(error, commands.errors.MissingPermissions):
-            embed = Embed(title=":x: Unable to give Blabby role",
-                          description="`Manage Roles` permission required "
-                          "to give `Blabby` role",
-                          colour=Colour.red())
+        # Check if member name was provided
+        if not member_name:
+            embed = Embed(
+                title=(":information_source: **No member to assign** `Blabby` "
+                       "**role to**"),
+                colour=Colour.blue())
+        else:
+            # Find member in guild
+            member = await commands.MemberConverter().convert(ctx, member_name)
+
+            # Ensure that Blabby role exists
+            role = utils.get(ctx.guild.roles, name="Blabby")
+            if not role:
+                role = await ctx.guild.create_role(name='Blabby')
+
+            await member.add_roles(role)
+
+            embed = Embed(
+                title=(f":white_check_mark: {member.display_name} has been "
+                        "assigned `Blabby` role"),
+                colour=Colour.green())
+        await ctx.send(embed=embed)
+    
+    @giveblabby.error
+    async def giveblabby_error(self, ctx, error):
+        """
+        Local error handler for Blabber's giveblabby command.
+
+        parameters:
+            ctx     [Context]: context object representing command invocation
+            error [Exception]: exception object raised from command function
+        """
+        embed = Embed(
+            title=f":x: **Unable to assign** `Blabby` **role**",
+            colour=Colour.red())
+        if isinstance(error, commands.BotMissingPermissions):
+            embed.description = ("Blabber does not have `Manage Roles` "
+                                 "permission")
+        elif isinstance(error, commands.MissingPermissions):
+            embed.description = ("`Manage Roles` permission required to use "
+                                 "this command")
+        else:
+            member_name = ctx.kwargs['member_name']
+            embed.description = f"`{member_name}` was not found in this server"
         await ctx.send(embed=embed)
 
 
 def setup(bot):
     """
-    Adds private _Roles Cog to bot.
+    Adds Roles Cog to bot.
 
     parameter: 
         bot [discord.Bot]: discord Bot object
     """
-    bot.add_cog(_Roles(bot))
+    bot.add_cog(Roles(bot))
